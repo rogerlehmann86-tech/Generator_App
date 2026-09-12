e304f634d174b7e316cb56accf2b1f606df8575c
rogerlehmann86-tech <rogerlehmann86@gmail.com>
2026-09-12 14:42:43 +0200

    Stage service worker v6.5.12

---
+++ .deploy-final/service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='generatorcalc-v6.5.12';
+const FILES=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.json","./lehmann-logo.png","./icon-192.png","./icon-512.png"];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
