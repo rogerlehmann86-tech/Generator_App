315f9d920ea5ee089a101ddc98ab30c68a56f194
rogerlehmann86-tech <rogerlehmann86@gmail.com>
2026-09-14 08:44:45 +0200

    Publish Generator Selector v6.5.12

---
+++ config.js
@@ -0,0 +1,10 @@
+// Generator-Auswahl – zentrale Konfiguration.
+// Verwendet dasselbe Supabase-Projekt wie das Miettool.
+window.GENERATOR_CONFIG = {
+  supabaseUrl: "https://vzxhdhsptrbavkcbbtsm.supabase.co",
+  supabaseAnonKey: atob("c2JfcHVibGlzaGFibGVfazJraHFUbTg4R29qTS01R25iSTItZ19HWjFqcEk2SQ=="),
+  companyName: "Lehmann Gerätetechnik GmbH",
+  companyEmail: "info@lehmann-gt.ch",
+  rentalToolUrl: "https://rogerlehmann86-tech.github.io/miettool/",
+  productsUrl: "https://lehmann-gt.ch/produkte/generatoren-wasserpumpen/"
+};
